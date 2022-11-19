65012477373da1f65e6bc76b443887f8d26b9230
José Silva <josenilo.cp@gmail.com>
2022-11-19 20:50:06 -0300

    Primeiro componente em react

--- 21 - react/exercicios/src/index.js
+++ 21 - react/exercicios/src/index.js
@@ -1,6 +1,9 @@
 import React from  'react'
 import ReadDOM from 'react-dom'
 
-const elemento = <h1>React 2</h1>
-ReadDOM.render(elemento, document.getElementById('root'))
+import Primeiro from './componentes/Primeiro'
+
+// const elemento = <h1>React 2</h1>
+// ReadDOM.render(elemento, document.getElementById('root'))
+ReadDOM.render(<Primeiro />, document.getElementById('root'))
 
