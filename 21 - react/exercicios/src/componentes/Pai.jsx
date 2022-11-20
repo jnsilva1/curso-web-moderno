c09758af8158496ea2dfa786366c99fa30371d93
José Silva <josenilo.cp@gmail.com>
2022-11-20 14:45:24 -0300

    Componentes Pai & Filho 02

--- 21 - react/exercicios/src/componentes/Pai.jsx
+++ 21 - react/exercicios/src/componentes/Pai.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import Filho from "./Filho";
+import { childrenWithProps } from "../utils/utils";
 
 const Pai = props =>
     <div>
         <h1>{props.nome} {props.sobrenome}</h1>
         <h2>Filhos</h2>
         <ul>
-            <Filho nome="Pedro" sobrenome={props.sobrenome}/>
-            <Filho {...props}/>
-            <Filho {...props} nome="Carla"/>
+            {childrenWithProps(props)}
         </ul>
     </div>
 
