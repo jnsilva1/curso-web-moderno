9c13d4e8a7a167eb389d5e42740e0d5cec664861
José Silva <josenilo.cp@gmail.com>
2022-11-20 13:45:46 -0300

    Multiplos Componentes

--- 21 - react/exercicios/src/index.js
+++ 21 - react/exercicios/src/index.js
@@ -1,9 +1,18 @@
 import React from  'react'
 import ReadDOM from 'react-dom'
 
-import BomDia from './componentes/BomDia'
+// import BomDia from './componentes/BomDia'
+
+import Multi, { BoaNoite} from './componentes/Multiplos'
 
 // const elemento = <h1>React 2</h1>
 // ReadDOM.render(elemento, document.getElementById('root'))
-ReadDOM.render(<BomDia nome="José Nilo" idade={25}/>, document.getElementById('root'))
+//ReadDOM.render(<BomDia nome="José Nilo" idade={25}/>, document.getElementById('root'))
+
+ReadDOM.render(
+<div>
+    <Multi.BoaTarde nome="Ana"/>
+    <BoaNoite nome="Bia"/>
+</div>,
+ document.getElementById('root'))
 
