601ebb2ca7c54f114581f888b33cbc5445fd7eb2
José Silva <josenilo.cp@gmail.com>
2022-11-20 14:15:02 -0300

    Componente de Classe 01

--- 21 - react/exercicios/src/index.js
+++ 21 - react/exercicios/src/index.js
@@ -3,16 +3,20 @@ import ReadDOM from 'react-dom'
 
 // import BomDia from './componentes/BomDia'
 
-import Multi, { BoaNoite} from './componentes/Multiplos'
+// import Multi, { BoaNoite} from './componentes/Multiplos'
 
 // const elemento = <h1>React 2</h1>
 // ReadDOM.render(elemento, document.getElementById('root'))
 //ReadDOM.render(<BomDia nome="José Nilo" idade={25}/>, document.getElementById('root'))
 
-ReadDOM.render(
-<div>
-    <Multi.BoaTarde nome="Ana"/>
-    <BoaNoite nome="Bia"/>
-</div>,
- document.getElementById('root'))
+// ReadDOM.render(
+// <div>
+//     <Multi.BoaTarde nome="Ana"/>
+//     <BoaNoite nome="Bia"/>
+// </div>,
+//  document.getElementById('root'))
 
+import Saudacao from './componentes/Saudacao'
+ReadDOM.render(
+    <Saudacao tipo="Bom dia" nome="João"/>,
+     document.getElementById('root'))
