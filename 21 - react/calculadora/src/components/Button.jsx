ce0dcf9153f6237975af5fa65da13ef09c6fbbbd
José Silva <josenilo.cp@gmail.com>
2022-11-21 21:33:36 -0300

    Componentes Button 02

--- 21 - react/calculadora/src/components/Button.jsx
+++ 21 - react/calculadora/src/components/Button.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import './Button.css'
 
-const Button = props => 
-    <button className="button">{props.label}</button>
+const Button = props => {
+    let classes = ['button']
+    if(props.operation) classes.push('operation')
+    if(props.double) classes.push('double')
+    if(props.triple) classes.push('triple')
+    return (
+        <button 
+            onClick={e=> props.click && props.click(props.label)}
+            className={`${classes.join(' ')}`}>
+            {props.label}
+        </button>
+    )
+}
+
 
 export default Button
