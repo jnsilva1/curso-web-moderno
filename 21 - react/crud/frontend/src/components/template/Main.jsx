578f4aeacdfa7d3644c38c10838649fda750f9e0
José Silva <josenilo.cp@gmail.com>
2022-11-23 21:18:01 -0300

    Componente Cabeçalho

--- 21 - react/crud/frontend/src/components/template/Main.jsx
+++ 21 - react/crud/frontend/src/components/template/Main.jsx
@@ -4,7 +4,7 @@ import Header from './Header'
 
 const Main = props =>
     <Fragment>
-        <Header/>
+        <Header {...props}/>
         <main className="content">
             Conteúdo
         </main>
