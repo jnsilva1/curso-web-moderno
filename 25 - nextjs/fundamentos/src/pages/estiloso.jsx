b61c77f7278db5fdd0956ce15d5f0997d9997961
José Silva <josenilo.cp@gmail.com>
2022-12-19 21:06:16 -0300

    CSS modularizado

---
+++ 25 - nextjs/fundamentos/src/pages/estiloso.jsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Estiloso.module.css'
+function Estiloso(){
+
+    return (
+        <div className={styles.roxo}>
+            <h1>Estilo usando CSS Módulos</h1>
+        </div>
+    )    
+}
+export default Estiloso
