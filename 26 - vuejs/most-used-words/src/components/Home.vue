c34e2ff16a8e2e9c33479e1ba35598ff7a3eecf3
José Silva <josenilo.cp@gmail.com>
2023-02-14 20:55:25 -0300

    Interface Gráfica com Vue JS #01

---
+++ 26 - vuejs/most-used-words/src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container fluid>
+    <div class="pills">
+        <Pill v-for="word in groupedWords" :key="word.name"
+           :name="word.name" :amount="word.amount"/>
+    </div>
+  </v-container>
+</template>
+
+<script>
+import Pill from './Pill.vue'
+export default {
+    name:'HomeDisplay',
+
+    components:{
+        Pill
+    },
+
+    data: ()=>({
+        groupedWords: [
+            {name:'I', amount: 1234 },
+            {name:'you', amount: 900 },
+            {name:'here', amount: 853 }
+        ]
+    })
+}
+</script>
+
+<style>
+
+</style>
---
+++ 26 - vuejs/most-used-words/src/components/Pill.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <span>{{name}}</span>
+    <span>{{amount}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'PillDisplay',
+
+    props:['name','amount']
+}
+</script>
+
+<style>
+
+</style>
