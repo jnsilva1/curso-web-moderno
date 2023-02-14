d956b2a59fa79464ba89ebe47ffb47ec72b653ca
José Silva <josenilo.cp@gmail.com>
2023-02-13 23:26:15 -0300

    Finalizado o projeto de Monty Hall

---
+++ 26 - vuejs/montyhall/src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="app">
+    <h1>Problema de Monty Hall</h1>
+    <div class="form">
+      <div v-if="!started">
+        <label for="doorAmount">Quantas portas?</label>
+        <input type="text" id="doorAmount" size="3"
+            v-model.number="doorAmount" />
+      </div>
+      <div v-if="!started">
+        <label for="selectedDoor">Qual porta é premiada?</label>
+        <input type="text" size="3" id="selectedDoor" v-model.number="selectedDoor">
+      </div>
+      <button v-if="!started" @click="started = true">Iniciar</button>
+      <button v-if="started" @click="started = false">Reiniciar</button>
+    </div>
+    <div class="doors" v-if="started">
+      <div v-for="i in doorAmount" :key="i">
+        <Door :hasGift="i === selectedDoor" :number="i" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Door from './components/Door'
+export default {
+  name:'App',
+  components: { Door },
+  data: function(){
+    return {
+      started: false,
+      doorAmount: 3,
+      selectedDoor: null
+    }
+  }
+}
+</script>
+
+<style>
+*{
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+}
+
+body{
+    color:#fff;
+    background: linear-gradient(to right, rgb(21,153,87), rgb(21,87,153));
+}
+
+#app{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+#app h1{
+    border: 1px solid #000;
+    background-color: #0004;
+    padding: 20px;
+    margin-bottom: 60px;
+}
+
+.form{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 40px;
+}
+
+.form, .form input, .form button{
+  margin-bottom: 10px;
+  font-size: 2rem;
+}
+
+.doors{
+  align-self: stretch;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+</style>
