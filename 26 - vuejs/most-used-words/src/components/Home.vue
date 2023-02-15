94f8c008bb76eaa4be92e15f3f76552ef3e88352
José Silva <josenilo.cp@gmail.com>
2023-02-14 21:14:49 -0300

    Interface Gráfica com Vue JS #03

--- 26 - vuejs/most-used-words/src/components/Home.vue
+++ 26 - vuejs/most-used-words/src/components/Home.vue
@@ -1,5 +1,17 @@
 <template>
   <v-container fluid>
+    <v-form>
+        <v-file-input 
+            label="Selecione as Legendas"
+            prepend-icon="mdi-message-text"
+            append-outer-icon="mdi-send"
+            outlined
+            multiple
+            chips
+            v-model="files"
+            @click:append-outer="processSubtitle"
+            />
+    </v-form>
     <div class="pills">
         <Pill v-for="word in groupedWords" :key="word.name"
            :name="word.name" :amount="word.amount"/>
@@ -17,12 +29,19 @@ export default {
     },
 
     data: ()=>({
+        files:[],
         groupedWords: [
             {name:'I', amount: 1234 },
             {name:'you', amount: 900 },
             {name:'he', amount: 853 },
         ]
-    })
+    }),
+
+    methods:{
+        processSubtitle(){
+            console.log(this.files)
+        }
+    }
 }
 </script>
 
