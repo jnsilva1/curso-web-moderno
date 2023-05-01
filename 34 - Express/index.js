4cde8a5f6878bb638d58a753d32c3fe64353b6ca
José Silva <josenilo.cp@gmail.com>
2023-05-01 15:20:24 -0300

    Middleware 02

--- 34 - Express/index.js
+++ 34 - Express/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+const saudacao = require("./saudacaoMid");
+
+app.use(saudacao("José Silva"));
+
 app.use((req, res, next) => {
   console.log("Antes...");
   next(); // irá rodar neste método (e nada mais)
