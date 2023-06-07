60b5140afbdd556db5abb336c39aeda169f99eb0
José Silva <josenilo.cp@gmail.com>
2023-06-07 20:37:38 -0300

    Category API

--- 35 - Knowledge/versao-inicial/backend/config/routes.js
+++ 35 - Knowledge/versao-inicial/backend/config/routes.js
@@ -5,6 +5,8 @@ module.exports = (app) => {
 
   app.route('/categories').get(app.api.category.get).post(app.api.category.save)
 
+  app.route('/categories/tree').get(app.api.category.getTree)
+
   app
     .route('/categories/:id')
     .get(app.api.category.getById)
