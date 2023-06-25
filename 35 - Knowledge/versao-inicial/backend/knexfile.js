bc59063810ae5cdceeeb00cd2b588cadb8bf3047
José Silva <josenilo.cp@gmail.com>
2023-06-25 19:03:24 -0300

    Adicionando as informações de conexões para o arqivo .env

--- 35 - Knowledge/versao-inicial/backend/knexfile.js
+++ 35 - Knowledge/versao-inicial/backend/knexfile.js
@@ -1,17 +1,12 @@
+const { db } = require('./.env')
 module.exports = {
-  client: "postgresql",
-  connection: {
-    host: "127.0.0.1",
-    port: 5432,
-    database: "knowledge",
-    user: "postgres",
-    password: "123456",
-  },
+  client: 'postgresql',
+  connection: db,
   pool: {
     min: 2,
     max: 10,
   },
   migrations: {
-    tableName: "knex_migrations",
+    tableName: 'knex_migrations',
   },
-};
+}
