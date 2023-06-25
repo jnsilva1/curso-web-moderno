38f85e60ea7b0e16892493793a6f981d3b77b953
José Silva <josenilo.cp@gmail.com>
2023-06-25 15:17:31 -0300

    API de Autenticação

--- 35 - Knowledge/versao-inicial/backend/api/user.js
+++ 35 - Knowledge/versao-inicial/backend/api/user.js
@@ -1,75 +1,68 @@
-const bcrypt = require("bcrypt-nodejs");
+const bcrypt = require('bcrypt-nodejs')
 
 module.exports = (app) => {
-  const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation;
+  const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation
 
   const encryptPassword = (password) => {
-    const salt = bcrypt.genSaltSync(10);
-    return bcrypt.hashSync(password, salt);
-  };
+    const salt = bcrypt.genSaltSync(10)
+    return bcrypt.hashSync(password, salt)
+  }
 
   const save = async (req, res) => {
-    const user = { ...req.body };
-    if (req.params.id) user.id = req.params.id;
+    const user = { ...req.body }
+    if (req.params.id) user.id = req.params.id
 
     try {
-      existsOrError(user.name, "Name not informed");
-      existsOrError(user.email, "E-mail not informed");
-      existsOrError(user.password, "Password not informed");
-      existsOrError(user.confirmPassword, "Password confirmation invalid");
-      equalsOrError(
-        user.password,
-        user.confirmPassword,
-        "Passwords do not match"
-      );
+      existsOrError(user.name, 'Name not informed')
+      existsOrError(user.email, 'E-mail not informed')
+      existsOrError(user.password, 'Password not informed')
+      existsOrError(user.confirmPassword, 'Password confirmation invalid')
+      equalsOrError(user.password, user.confirmPassword, 'Passwords do not match')
 
-      const userFromDB = await app
-        .db("users")
-        .where({ email: user.email })
-        .first();
+      const userFromDB = await app.db('users').where({ email: user.email }).first()
       if (!user.id) {
-        notExistsOrError(userFromDB, "User already registered");
+        notExistsOrError(userFromDB, 'User already registered')
       }
     } catch (msg) {
-      return res.status(400).send(msg);
+      return res.status(400).send(msg)
     }
 
-    user.password = encryptPassword(user.password);
-    delete user.confirmPassword;
+    user.password = encryptPassword(user.password)
+    delete user.confirmPassword
 
     if (user.id) {
       app
-        .db("users")
+        .db('users')
         .update(user)
         .where({ id: user.id })
         .then((_) => res.status(204).send())
-        .catch((err) => res.status(500).send(err));
+        .catch((err) => res.status(500).send(err))
     } else {
       app
-        .db("users")
+        .db('users')
         .insert(user)
         .then((_) => res.status(204).send())
-        .catch((err) => res.status(500).send(err));
+        .catch((err) => res.status(500).send(err))
     }
-  };
+  }
 
   const get = (req, res) => {
     app
-      .db("users")
-      .select("id", "name", "email", "admin")
+      .db('users')
+      .select('id', 'name', 'email', 'admin')
       .then((users) => res.json(users))
-      .catch((err) => res.status(500).send(err));
-  };
+      .catch((err) => res.status(500).send(err))
+  }
 
   const getById = (req, res) => {
     app
-      .db("users")
-      .select("id", "name", "email", "admin")
+      .db('users')
+      .select('id', 'name', 'email', 'admin')
       .where({ id: req.params.id })
       .first()
       .then((user) => res.json(user))
-      .catch((err) => res.status(500).send(err));
-  };
+      .catch((err) => res.status(500).send(err))
+  }
 
-  return { save, get, getById };
-};
+  return { save, get, getById }
+}
