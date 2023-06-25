a34b3513ceca74d9c25c1bb07524245b1bd6daae
José Silva <josenilo.cp@gmail.com>
2023-06-25 19:12:14 -0300

    validando o cadastro administrador

--- 35 - Knowledge/versao-inicial/backend/api/user.js
+++ 35 - Knowledge/versao-inicial/backend/api/user.js
@@ -12,6 +12,9 @@ module.exports = (app) => {
     const user = { ...req.body }
     if (req.params.id) user.id = req.params.id
 
+    //Usuário criado a partiur do sign-up
+    if (!req.originalUrl.startsWith('/users')) user.admin = false
+    if (!req.user || !req.user.admin) user.admin = false
     try {
       existsOrError(user.name, 'Name not informed')
       existsOrError(user.email, 'E-mail not informed')
