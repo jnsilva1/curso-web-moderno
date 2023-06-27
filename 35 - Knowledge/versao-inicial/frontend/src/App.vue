7f947b274a8ec88f3e3655234ff6893d06ba3814
José Silva <josenilo.cp@gmail.com>
2023-06-27 20:51:08 -0300

    Estrutura Inicial do Template

--- 35 - Knowledge/versao-inicial/frontend/src/App.vue
+++ 35 - Knowledge/versao-inicial/frontend/src/App.vue
@@ -1,15 +1,47 @@
 <template>
 	<div id="app">
-		<h1>Versão Inicial</h1>
+		<Header />
+		<Menu />
+		<Content />
+		<Footer />
 	</div>
 </template>
 
 <script>
+import Header from '@/components/template/Header.vue'
+import Menu from '@/components/template/Menu.vue'
+import Footer from '@/components/template/Footer.vue'
+import Content from '@/components/template/Content.vue'
+
 export default {
-	name: "App",
+  name: 'App',
+  components: {
+    Header,
+    Menu,
+    Content,
+    Footer,
+  },
 }
 </script>
 
 <style>
+* {
+  font-family: 'Lato', sans-serif;
+}
+body {
+  margin: 0;
+}
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 60px 1fr 40px;
+  grid-template-columns: 300px 1fr;
+  grid-template-areas:
+    'header header header'
+    'menu content content'
+    'footer footer footer';
+}
 </style>
---
+++ 35 - Knowledge/versao-inicial/frontend/src/components/template/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="footer">
+    <span>Copyright <strong>Cod3r & José Silva</strong> &copy; 2015-2018  </span>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style>
+.footer {
+  grid-area: footer;
+  background-color: #ddd;
+  color: #333;
+
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  padding-right: 10px;
+}
+</style>
