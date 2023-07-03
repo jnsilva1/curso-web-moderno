e472ec512519b8cf64a166d8a70b6c3253dfe8fc
José Silva <josenilo.cp@gmail.com>
2023-07-03 17:45:56 -0300

    Cadastro de usuário 01

--- 35 - Knowledge/versao-inicial/frontend/src/components/admin/UserAdmin.vue
+++ 35 - Knowledge/versao-inicial/frontend/src/components/admin/UserAdmin.vue
@@ -1,12 +1,50 @@
 <template>
   <div class="user-admin">
-    <h1>Usuário Componente</h1>
+    <b-table hover stripped :items="users" :fields="fields">
+
+    </b-table>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { baseApiUrl } from '@/global'
 export default {
   name: 'UserAdmin',
+  data() {
+    return {
+      mode: 'save',
+      user: {},
+      users: [],
+      fields: [
+        { key: 'id', label: 'Código', sortable: true },
+        { key: 'name', label: 'Nome', sortable: true },
+        { key: 'email', label: 'E-mail', sortable: true },
+        {
+          key: 'admin',
+          label: 'Administrador',
+          sortable: true,
+          formatter: (value) => (value ? 'Sim' : 'Não'),
+        },
+        {
+          key: 'actions',
+          label: 'Ações',
+          class: 'text-center',
+          thClass: 'text-center',
+        },
+      ],
+    }
+  },
+  methods: {
+    loadUsers() {
+      axios.get(`${baseApiUrl}/users`).then(({ data }) => {
+        this.users = data
+      })
+    },
+  },
+  mounted() {
+    this.loadUsers()
+  },
 }
 </script>
 
