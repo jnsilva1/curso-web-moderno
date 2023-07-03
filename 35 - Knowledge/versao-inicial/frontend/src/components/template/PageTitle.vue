ee025cd97e325351a8f038da7b6f00e10dfcf426
José Silva <josenilo.cp@gmail.com>
2023-07-03 16:27:14 -0300

    Componente Título da Página

---
+++ 35 - Knowledge/versao-inicial/frontend/src/components/template/PageTitle.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="page-title">
+    <h1>
+        <i v-if="icon" :class="icon">{{ main }}</i>
+    </h1>
+    <h2>
+        {{ sub }}
+    </h2>
+    <hr/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageTitle',
+  props: {
+    main: String,
+    sub: String,
+    icon: String,
+  },
+}
+</script>
+
+<style>
+.page-title {
+  margin: 0;
+}
+.page-title h2 {
+  color: #777;
+  margin-top: 5px;
+  font-size: 1.3rem;
+}
+</style>
